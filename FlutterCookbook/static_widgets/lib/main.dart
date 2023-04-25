ea6eb453ea285112f69b24fbc1c6dfe53654f04a
Христюк Игорь Анатольевич <ikhristiuk@gmail.com>
2023-04-25 18:56:54 +0300

    basic screen, scaffold, drawer

--- FlutterCookbook/static_widgets/lib/main.dart
+++ FlutterCookbook/static_widgets/lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-
-import 'immutable_widget.dart';
+import 'package:static_widgets/basic_screen.dart';
 
 void main() {
   runApp(StaticApp());
 }
 
 class StaticApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ImmutableWidget(),
+      home: BasicScreen(),
     );
   }
 }
