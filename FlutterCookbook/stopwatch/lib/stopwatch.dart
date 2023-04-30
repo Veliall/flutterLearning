48c3c1f47033a5afee32cf8e532afa7ca44ac315
Христюк Игорь Анатольевич <ikhristiuk@gmail.com>
2023-04-30 20:13:21 +0300

    stopwatch laps

--- FlutterCookbook/stopwatch/lib/stopwatch.dart
+++ FlutterCookbook/stopwatch/lib/stopwatch.dart
@@ -13,6 +13,7 @@ class _StopWatchState extends State<StopWatch> {
   bool isTicking = false;
   int milliseconds = 0;
   late Timer timer;
+  final laps = <int>[];
 
   void _onTick(Timer time) {
     setState(() {
@@ -27,40 +28,76 @@ class _StopWatchState extends State<StopWatch> {
         title: const Text('Stopwatch'),
       ),
       body: Column(
+        children: [
+          Expanded(child: _buildCounter(context)),
+          Expanded(child: _buildLapDisplay())
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            _secondsTex(),
-            style: Theme.of(context).textTheme.headlineMedium,
+            'Lap ${laps.length + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium
+                ?.copyWith(color: Colors.white),
+          ),
+          Text(
+            _secondsTex(milliseconds),
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(color: Colors.white),
           ),
           const SizedBox(
             height: 20,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green),
-                  foregroundColor: MaterialStateProperty.all(Colors.white),
-                ),
-                onPressed: () => _startTimer(),
-                child: const Text('Start'),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.red),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                  ),
-                  onPressed: () => _stopTimer(),
-                  child: const Text('Stop'))
-            ],
-          )
+          _buildControls()
         ],
       ),
+    );
+  }
+
+  Widget _buildControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+          ),
+          onPressed: () => _startTimer(),
+          child: const Text('Start'),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.yellow),
+          ),
+          onPressed: () => isTicking ? _lap() : null,
+          child: const Text('Lap'),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+              foregroundColor: MaterialStateProperty.all(Colors.white),
+            ),
+            onPressed: () => _stopTimer(),
+            child: const Text('Stop'))
+      ],
     );
   }
 
@@ -72,6 +109,7 @@ class _StopWatchState extends State<StopWatch> {
     setState(() {
       milliseconds = 0;
       isTicking = true;
+      laps.clear();
     });
   }
 
@@ -85,9 +123,27 @@ class _StopWatchState extends State<StopWatch> {
     });
   }
 
-  String _secondsTex() {
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  String _secondsTex(int milliseconds) {
     final seconds = milliseconds / 1000;
     return '$seconds seconds';
+  }
+
+  Widget _buildLapDisplay() {
+    return ListView(
+      children: [
+        for (int milliseconds in laps)
+          ListTile(
+            title: Text(_secondsTex(milliseconds)),
+          ),
+      ],
+    );
   }
 
   @override
