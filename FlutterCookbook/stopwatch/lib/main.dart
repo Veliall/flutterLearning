e7b5d21eddbf67ec59c16196774a46cc37cae2a5
Veliall <me@jk-dev.ru>
2023-05-02 20:23:27 +0300

    stopwatch logging

--- FlutterCookbook/stopwatch/lib/main.dart
+++ FlutterCookbook/stopwatch/lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:stopwatch/stopwatch.dart';
+import 'package:stopwatch/login_screen.dart';
 
 void main() => runApp(StopwatchApp());
 
 class StopwatchApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: StopWatch(),
+      home: LoginScreen(),
     );
   }
 }
